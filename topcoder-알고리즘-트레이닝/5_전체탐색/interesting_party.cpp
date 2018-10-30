6a1aba4266fe0aed9f55c601a20c05a5cf65487f
Ji Seok Lee <jiseoklee0502@gmail.com>
2018-10-30 22:03:24 +0900

    Interesting Party Problem

---
+++ topcoder-알고리즘-트레이닝/5_전체탐색/interesting_party.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <iostream>
+
+class InterestingParty {
+public:
+    int bestInvitation(vector <string> first, vector<string> second) {
+        map <string,int> dic;
+
+        for (int i=0; i<first.size(); i++) {
+            dic[first[i]] = 0;
+            dic[second[i]] = 0;
+        }
+
+        for (int i=0; i<first.size(); i++) {
+            dic[first[i]]++;
+            dic[second[i]]++;
+        }
+
+        int ans = 0;
+        map <string,int>::iterator it;
+
+        for(it=dic.begin(); it != dic.end(); it++) {
+            ans = max(ans, it->second);
+        }
+
+        return ans;
+    }
+};
