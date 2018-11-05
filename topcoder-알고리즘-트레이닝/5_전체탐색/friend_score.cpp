06396c8057bf24ac2e6a9016b7a9d94f57068e6c
Ji Seok Lee <jiseoklee0502@gmail.com>
2018-11-05 23:05:22 +0900

    Friend Score Problem

---
+++ topcoder-알고리즘-트레이닝/5_전체탐색/friend_score.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class FriendScore {
+public:
+    int highestScore(vector <string> friends) {
+        int ans = 0;
+        int n = friends[0].length();
+
+        for (int i=0; i<n; i++) {
+            int cnt = 0;
+
+            for (int j=0; j<n; j++) {
+                if (i == j) continue;
+
+                if (friends[i][j] == 'Y') {
+                    cnt++;
+                } else {
+                    for (int k=0; k<n; k++) {
+                        if (friends[j][k] == 'Y' && friends[k][i] == 'Y') {
+                            cnt++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
