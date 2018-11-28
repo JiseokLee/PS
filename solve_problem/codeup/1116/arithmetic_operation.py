a0b3dbe7392bdc13d9b685da1fb886a710513278
Ji Seok Lee <jiseoklee0502@gmail.com>
2018-11-28 16:48:14 +0900

    Codeup 1116 - 사칙연산 계산기

---
+++ solve_problem/codeup/1116/arithmetic_operation.py
@@ -0,0 +1,9 @@
+
+num1, num2 = input().split()
+num1 = int(num1)
+num2 = int(num2)
+
+print('%d+%d=%d' % (num1, num2, int(num1+num2)))
+print('%d-%d=%d' % (num1, num2, int(num1-num2)))
+print('%d*%d=%d' % (num1, num2, int(num1*num2)))
+print('%d/%d=%d' % (num1, num2, int(num1/num2)))
