7c16531ef5c51dd77a68eed9362521c73135dc76
Ji Seok Lee <jiseoklee0502@gmail.com>
2018-11-30 15:51:06 +0900

    Codeup 1118 - 삼각형의 넓이 구하기

---
+++ solve_problem/codeup/1118/triangle_width.py
@@ -0,0 +1,8 @@
+
+bottom, height = input().split()
+bottom = float(bottom)
+height = float(height)
+
+width = bottom * height / 2
+
+print('%.1f' % width)
---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+	vector<int> a;
+	for(int i = 0; i < n; i++) {
+		a.push_back(i);
+	}
+	for(int i = 0; i < n; i++) {
+		cout << a[i] << ' ';
+	}
+	return 0;
+}
