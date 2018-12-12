3d3612872b1b626cdf1efe7e31d37974050dec52
Ji Seok Lee <jiseoklee0502@gmail.com>
2018-12-12 16:51:01 +0900

    Codeup solve problem
    Codeup 1155 - 7의 배수
    Codeup 1156 - 홀수 짝수 구별

---
+++ solve_problem/codeup/1155/multiple_of_7.py
@@ -0,0 +1,8 @@
+
+num = input()
+num = int(num)
+
+if(num % 7 == 0):
+    print('multiple')
+else:
+    print('not multiple')
---
+++ solve_problem/codeup/1156/distinction_odd_even.py
@@ -0,0 +1,8 @@
+
+num = input()
+num = int(num)
+
+if(num % 2 == 0):
+    print('even')
+else:
+    print('odd')
